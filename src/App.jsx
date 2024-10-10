f5e76a3dd91b89418d849c614a68374aab6ddf24
Duarte <durtemendonca1234@gmail.com>
2024-10-10 12:13:13 +0100

    Login form and verification
    Added a form for user login. Fetched the data of the users. Verified if the user exists in the database.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const[users,setUsers] = useState ([])
+  const[posts,setPosts] = useState ([])
+  const[username,setUsername] = useState("")
+  const[password,setPassword] = useState("")
+
+  async function getUsers(){
+    try {
+      const response = await fetch("http://localhost:5000/users");
+      const result = await response.json();
+      setUsers(result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+    
+  }
+
+  useEffect(() =>{
+    getUsers()
+  },[])
+
+  // Function to handle login verification
+  function verifyLogin(e) {
+    e.preventDefault(); 
+    
+    const user = users.find(user => user.email === username);
+
+    if (user) {
+  
+      if (user.password === password) {
+        console.log('Login successful!');
+       
+      } else {
+        console.log('Incorrect password');
+        
+      }
+    } else {
+      console.log('User not found');
+    }
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={verifyLogin}>
+        <div>
+          <label>Email</label>
+          <input type="email" value={username} onChange={e => setUsername(e.target.value)}></input>
+
+          <label>Password</label>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)}></input>
+        </div>
+        <button>Login</button>
+      </form>
+   
     </>
   )
 }
