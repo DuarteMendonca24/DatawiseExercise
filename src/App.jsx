5b568f22aae5d275f7a6d25f30b1b3e8d687b1a9
Duarte <durtemendonca1234@gmail.com>
2024-10-10 14:45:47 +0100

    Get posts and react-router-dom
    Added code to get the posts if the login is successful. Explored and installed react-rouder-dom to navigate between pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App () {
   const[posts,setPosts] = useState ([])
   const[username,setUsername] = useState("")
   const[password,setPassword] = useState("")
+  const[isLoggedIn,setIsLoggedIn] = useState(false)
 
   async function getUsers(){
     try {
@@ -22,6 +23,18 @@ function App () {
     getUsers()
   },[])
 
+  async function getPosts(id){
+    try {
+      const response = await fetch(`http://localhost:5000/posts?userId=${id}`);
+      const result = await response.json();
+      setPosts(result);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+  
+  console.log(posts)
+
   // Function to handle login verification
   function verifyLogin(e) {
     e.preventDefault(); 
@@ -32,7 +45,10 @@ function App () {
   
       if (user.password === password) {
         console.log('Login successful!');
-       
+        setIsLoggedIn(true)
+        getPosts(user.id)
+        
+      
       } else {
         console.log('Incorrect password');
         
@@ -49,7 +65,7 @@ function App () {
         <div>
           <label>Email</label>
           <input type="email" value={username} onChange={e => setUsername(e.target.value)}></input>
-
+          
           <label>Password</label>
           <input type="password" value={password} onChange={e => setPassword(e.target.value)}></input>
         </div>
